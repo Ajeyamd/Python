b7800bc3ff9f1fbe0ba22bf3fba2aa5f0c1a3834
Ajeya <ajeyamd3@gmail.com>
2023-05-06 23:31:17 +0530

    uploaded

---
+++ palindrome.py
@@ -0,0 +1,8 @@
+def isPalindrome(strng):
+    if len(strng) == 0:
+        return 'Palindrome'
+    if strng[0] != strng[len(strng)-1]:
+        return 'Not Palindrome'
+    return isPalindrome(strng[1:-1])
+s='kayakak'
+print(isPalindrome(s))
