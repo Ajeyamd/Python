ae7ed6a366584d65ecce16dbb0521951f0990d1f
Ajeya <ajeyamd3@gmail.com>
2023-05-08 22:38:51 +0530

    updated

---
+++ arrFlatten.py
@@ -0,0 +1,13 @@
+def flatten(arr):
+    res = []
+    for i in arr:
+        if type(i) is list:
+            res.extend(flatten(i))
+        else:
+            res.append(i)
+    return res
+
+print(flatten([1,2,3,[4,5],6,7]))
+
+
+        
