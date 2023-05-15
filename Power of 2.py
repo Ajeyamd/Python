1b08015fa162d396987434c298dfea99e5b47eb9
Ajeya <ajeyamd3@gmail.com>
2023-05-15 15:37:54 +0530

    updated

--- Power of 2.py
+++ Power of 2.py
@@ -1,16 +1,7 @@
-# def power(n):
-#     i=0
-#     power=1
-#     while i<n:
-#         power=power*2
-#         i=i+1
-#     return power
-# print(power(3))
-
 def power(n):
     if n==0:
         return 1
     else:
         p=power(n-1)
     return p*2
-print(power(3))
+print(power(7))
